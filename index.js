1b748cc82735bb5f30f95b9a293dcf56ee6d9bc1
ChangHyun-S <lkisa5@gmail.com>
2021-01-28 19:38:42 +0900

    add register

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyPasrser = require("body-parser");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded 
+app.use(bodyPasrser.urlencoded({extended: true}));
+// application/json 분석해서 가져오는거
+app.use(bodyPasrser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://lkisa5:1q2w3e4r@boilerplate.j4ppf.mongodb.net/boilerplate?retryWrites=true&w=majority', {
@@ -13,6 +20,21 @@ mongoose.connect('mongodb+srv://lkisa5:1q2w3e4r@boilerplate.j4ppf.mongodb.net/bo
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!!!!!!!!!!!!!!')
+})
+
+
+  
+app.post("/register",(req, res)=> {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다
+  const user = new User(req.body)
+
+  user.save((err, userInfo)=> {
+      if (err) return res.json({success: false, err})
+      return res.status(200).json({
+        success: true
+    })
+  })
 })
 
 app.listen(port, () => {
