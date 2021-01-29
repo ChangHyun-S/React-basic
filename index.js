adb52cf2b5249532e4410bd4c3e18ba9d08ff21b
ChangHyun-S <lkisa5@gmail.com>
2021-01-29 22:04:45 +0900

    add jsonwebtoken

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyPasrser = require("body-parser");
+const cookieParser = require('cookie-parser')
 const { User } = require("./models/User");
 const config = require('./config/key')
 
 // application/x-www-form-urlencoded 
-app.use(bodyPasrser.urlencoded({extended: true}));
+app.use(bodyPasrser.urlencoded({ extended: true }));
+
 // application/json 분석해서 가져오는거
 app.use(bodyPasrser.json());
+app.use(cookieParser());
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { request } = require('express');
 mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
@@ -25,15 +29,46 @@ app.get('/', (req, res) => {
 
 
 // register route
-app.post("/register",(req, res)=> {
+app.post("/register", (req, res) => {
   // 회원 가입 할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터베이스에 넣어준다
   const user = new User(req.body)
 
-  user.save((err, userInfo)=> {
-      if (err) return res.json({success: false, err})
-      return res.status(200).json({
-        success: true
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다."
+        })
+      }
+
+      // 비밀번호까지 맞다면 토큰 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err)
+
+        // 토큰을 쿠키에 저장
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
     })
   })
 })
--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const { request } = require('express');
 // saltrounds salt 글자수
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -35,16 +37,16 @@ const userSchema = mongoose.Schema({
 })
 
 // user.save 전 실행
-userSchema.pre('save', function(next) {
+userSchema.pre('save', function (next) {
     var user = this
 
-    if(user.isModified('password')) {
+    if (user.isModified('password')) {
         // 비밀번호 암호화
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            if(err) return next(err)
-            
-            bcrypt.hash(user.password, salt, function(err, hash) {
-                if(err) return next(err)
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
                 // hash 비밀번호로 변경
                 user.password = hash
                 next()
@@ -56,6 +58,26 @@ userSchema.pre('save', function(next) {
     }
 })
 
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    // plainPassword
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function (cb) {
+    var user = this
+    // jsonwebtoken 이용해서 token 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
-module.exports = {User}
+module.exports = { User }
