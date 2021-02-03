2eefc182471b6fb3fab71238ea4263af4aa935d1
ChangHyun-S <lkisa5@gmail.com>
2021-02-03 11:17:46 +0900

    reducer #28

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux'
+import user from './user_reducer'
+
+const rootReducer = combineReducers({
+    // user,
+})
+
+// 다른 파일에서 reducer 쓸 수 있게 export
+export default rootReducer
