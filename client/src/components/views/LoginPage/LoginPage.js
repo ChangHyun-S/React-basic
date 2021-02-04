89c26504172d0cad482a57dfe50928926490569c
ChangHyun-S <lkisa5@gmail.com>
2021-02-04 18:51:59 +0900

    add loginpage state #30

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginPage() {
+    // state
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault()
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+            onSubmit = {onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler}></input>
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler}></input>
+                <br />
+                <button type="submit">
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
