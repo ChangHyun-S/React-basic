71f779839c7a0aab877eda4848f7aaa307237677
ChangHyun-S <lkisa5@gmail.com>
2021-02-05 17:31:07 +0900

    Add Logout return

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,32 +1,37 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
-import { response } from 'express';
 
-function LandingPage() {
-    useEffect(() => {
-        axios.get("/api/hello").then((response) => {
-          console.log(response);
-        });
-      }, []);
-    
-    const oncClickHandler = () => {
-      axios.get('/api/users/logout')
+function LandingPage(props) {
+  useEffect(() => {
+    axios.get("/api/hello").then((response) => {
+      console.log(response);
+    });
+  }, []);
+
+  const oncClickHandler = () => {
+    axios.get('/api/users/logout')
       .then(response => {
-        console.log(response.data)
+        if (response.data.success) {
+          props.history.push('/login')
+        }
+        else {
+          alert('Fail Logout')
+        }
       })
-    }
+  }
 
-    return (
-      <div style = {{
-        display: 'flex', justifyContent: 'center', alignItems: 'center',
-        width: '100%', height: '100vh'
+
+  return (
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      width: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
       <button onClick={oncClickHandler}>
         로그아웃
       </button>
-      </div>
-    )
+    </div>
+  )
 }
 
 export default LandingPage
